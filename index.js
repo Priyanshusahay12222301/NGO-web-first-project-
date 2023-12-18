b27d0e3f8ac2624f351b3a81cbcea22616339930
Priyanshusahay12222301 <119564402+Priyanshusahay12222301@users.noreply.github.com>
2023-12-18 12:53:12 +0530

    this is my first project

---
+++ index.js
@@ -0,0 +1,100 @@
+    //console.log("Hello world");
+    //alert("me");
+    //document.write("this is document");
+    //console.warn("this is warning");
+   // console.error("this is an error");
+
+//3. Java script variables
+//containers to store data variables
+
+
+
+/*var number1 = 34;
+var number2 = 56;
+console.log(number1 + number2);
+
+//4.data types in Javascript
+var str1 = "This is a string";
+var str2 = "This is also a string";
+ console.log(str1 , str2);
+
+ //objects
+ var marks = {
+    ravi: 34,
+    subham: 78,
+    harry: 99.977
+ }
+ console.log(marks);
+  var a = true;
+  var b = false;
+
+  console.log(a,b)
+
+  var und = undefined;
+  console.log(undefined);
+
+  //At a very high level ther are two type of data types in java Script.
+  
+var arr = [1,2,3,4,5]
+console.log(arr);
+console.log(arr[2])*/
+
+/*var age = 34;
+if(age > 8){
+    console.log("you are not a kid")
+}
+else {
+    console.log("you are a kid")
+}*/
+
+/*var arr = [1,2,3,4,5];
+console.log(arr);
+for(var i = 0;i<arr.length;i++){
+    console.log(arr[i])
+}*/
+
+/*arr.forEach(function(element){
+    console.log(element)
+})*/
+
+/*Let j=0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j ++;
+}*/
+
+/*var arr = [1,2,3,4,5,6,7]
+for(var i=0;i<arr.length;i++){
+    if(i==2){
+        continue;
+    }
+    console.log(arr[i])
+}*/
+
+/*let myArr = ["Fan","Camera", 34, nul, tree];
+console.log(myArr.length);
+//string methods in java script.*/
+/*let mydata = new DataTransfer();
+console.log(mydata.gettime());
+console.log(mydata.getfullyear());*/
+
+//let elemClass = document.getElementById('click');
+//console.log(elem);
+//et elemClass = document.getElementById('click');
+//console.log(gec);
+
+//function clicked() {
+  //  console.log('The button was clicked');
+//}
+/*window.onload = function(){
+    console.log('The document was loaded')
+}
+firstcontainer.addEventListner('click',function(){
+    console.log("click hua")
+})*/
+
+obj = {name: "harry",length : 1}
+jso = JSON.stringify(obj);
+console.log(typeof jso)
+console.log(jso)
+parsed = JSON.parse("{"name)
